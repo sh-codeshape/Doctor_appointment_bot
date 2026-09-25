cb817bb6e781dcccffe233f4e1cc2b7f1cfa32c0
Rudresh <rslikefoot00@gmail.com>
2026-09-25 14:39:01 +0530

    added deployment config

--- apps/api/db/migrations/0018_remnaming_rmo.sql
+++
@@ -1,8 +0,0 @@
--- migrate:up
-
-ALTER TABLE departments RENAME_COLUMN 'RMO - Resident Medical Officer' to 'General Physician'
-
-
--- migrate:down
-
-ALTER TABLE departments RENAME_COLUMN 'General Physician' to 'RMO - Resident Medical Officer'
